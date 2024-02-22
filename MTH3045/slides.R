71880e4027087aded4f58ae8aa284e0ac099056e
byoungman <45686107+byoungman@users.noreply.github.com>
2024-02-22 10:01:20 +0000

    Update slides.R

--- MTH3045/slides.R
+++ MTH3045/slides.R
@@ -10,18 +10,34 @@ hilbert <- function(n) {
 
 # operating temperatures
 temp <- c(35.3, 29.7, 30.8, 58.8, 61.4, 71.3, 74.4, 76.7, 70.7, 57.5,
-46.4, 28.9, 28.1, 39.1, 46.8, 48.5, 59.3, 70, 70, 74.5, 72.1,
-58.1, 44.6, 33.4, 28.6)
+          46.4, 28.9, 28.1, 39.1, 46.8, 48.5, 59.3, 70, 70, 74.5, 72.1,
+          58.1, 44.6, 33.4, 28.6)
 # number of operational days
 days <- c(20, 20, 23, 20, 21, 22, 11, 23, 21, 20, 20, 21, 21, 19, 23,
-20, 22, 22, 11, 23, 20, 21, 20, 20, 22)
+          20, 22, 22, 11, 23, 20, 21, 20, 20, 22)
 # output from factory
 output <- c(10.98, 11.13, 12.51, 8.4, 9.27, 8.73, 6.36, 8.5, 7.82, 9.14,
-8.24, 12.19, 11.88, 9.57, 10.94, 9.58, 10.09, 8.11, 6.83, 8.88,
-7.68, 8.47, 8.86, 10.36, 11.08)
+            8.24, 12.19, 11.88, 9.57, 10.94, 9.58, 10.09, 8.11, 6.83, 8.88,
+            7.68, 8.47, 8.86, 10.36, 11.08)
 # data frame
 prod <- data.frame(temp = temp, days = days, output = output)
 
 y <- c(.7, 1.3, 2.6)
 mu <- 1:3
 Sigma <- matrix(c(4, 2, 1, 2, 3, 2, 1, 2, 2), 3, 3)
+
+dmvn3 <- function(y, mu, Sigma, log = TRUE) {
+  # Function to evaluate MVN pdf
+  # y, mu vectors
+  # Sigma matrix
+  # log is logical indicating whether logarithm
+  # returns a scalar
+  p <- length(y)
+  res <- y - mu
+  L <- t(chol(Sigma))
+  out <- - sum(log(diag(L))) - 0.5 * p * log(2 * pi) -
+    0.5 * sum(forwardsolve(L, res)ˆ2)
+  if (!log)
+    out <- exp(out)
+  out
+}
