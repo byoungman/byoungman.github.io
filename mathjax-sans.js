e261a73e2a46e55f86b392c227e766bbd11c4bbb
byoungman <45686107+byoungman@users.noreply.github.com>
2025-07-01 09:34:49 +0100

    Create mathjax-sans.js

---
+++ mathjax-sans.js
@@ -0,0 +1,14 @@
+MathJax.Hub.Config({
+  "HTML-CSS": {
+    scale: 100,
+    availableFonts: [],
+    webFont: "STIX-Web",
+    imageFont: null,
+    styles: {
+      ".MathJax": {
+        "font-family": "sans-serif !important"
+      }
+    }
+  }
+});
+
